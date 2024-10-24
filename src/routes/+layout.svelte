2cf4cf3f36005e76adcbe9932614d32e253a8a70
fleimisch <bankgithub@skiff.com>
2024-10-24 17:23:59 +0200

    analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,13 @@
 <script lang="ts">
 	import '../app.css';
 	import { inject } from '@vercel/analytics';
+	import { onMount } from 'svelte';
 
 	let { children } = $props();
+
+	onMount(async () => {
+		inject();
+	});
 </script>
 
 {@render children()}
