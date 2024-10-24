7635d32ef9de706f6b087287a6adce22b909b093
fleimisch <osnejc@gmail.com>
2024-10-24 11:43:14 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# xbg-pledge-rewards-calculator
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/fleimisch/xbg-pledge-rewards-calculator)
---
+++ src/lib/components/RewardStats.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  export let label: string;
+  export let value: number;
+  export let color: string = "text-green-400";
+</script>
+
+<div>
+  <h3 class="text-lg font-semibold mb-2">{label}</h3>
+  <p class="text-2xl {color}">{value ? value.toFixed(2) : '0.00'}%</p>
+</div>
